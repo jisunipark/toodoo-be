6a63bb6af83b8f04230a68534f08e263ea4bffcb
Jisun Park <jisunpark.t@gmail.com>
2024-12-05 21:38:27 +0900

    feat: cors 패키치 설치 및 설정

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 const todos = [
   {
