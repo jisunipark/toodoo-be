a5b81c6fa0443f599eaf0b33a7921e071ebe8292
Jisun Park <jisunpark.t@gmail.com>
2024-12-05 16:02:33 +0900

    refactor: crud 리팩터링

--- server.js
+++ server.js
@@ -5,65 +5,68 @@ const port = 3000;
 
 app.use(express.json());
 
-let todos = [
+const todos = [
   {
-    id: 'e7ec',
+    id: 1,
     checked: false,
     todo: '자바스크립트 공부하기',
     date: '24. 12. 4.',
   },
   {
-    id: '5700',
+    id: 2,
     checked: false,
     todo: '알고리즘 강의 듣기',
     date: '24. 12. 4.',
   },
   {
-    id: '3c1e',
+    id: 3,
     checked: false,
     todo: '투두리스트 만들기',
     date: '24. 12. 4.',
   },
 ];
 
-app.get('/todos', (req, res) => {
-  res.status(200).json(todos);
+app.get('/api/todos', (req, res) => {
+  res.status(200).send(todos);
 });
 
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id;
-  const todo = todos.find((todo) => todo.id === id);
-  if (todo) res.status(200).json(todo);
-  else res.status(404).json({ error: 'Todo not found', message: '해당 todo를 찾을 수 없습니다.' });
+app.get('/api/todos/:id', (req, res) => {
+  const parsedId = parseInt(req.params.id);
+  const findTodo = todos.find((todo) => todo.id === parsedId);
+  if (isNaN(parsedId)) return res.status(400).send({ message: 'Bad Request. Invalid ID.' });
+  if (!findTodo) return res.sendStatus(404);
+  else return res.status(200).send(findTodo);
 });
 
-app.post('/todos', (req, res) => {
-  const newTodo = req.body;
+app.post('/api/todos', (req, res) => {
+  const newTodo = {
+    id: todos[todos.length - 1].id + 1,
+    ...req.body,
+  };
   todos.push(newTodo);
-  res.status(200).json(newTodo);
+  res.status(200).send(newTodo);
 });
 
-app.patch('/todos/:id', (req, res) => {
-  const id = req.params.id;
-  const targetTodo = todos.find((todo) => todo.id === id);
-  for (let key in req.body) {
-    targetTodo[key] = req.body[key];
-  }
-  todos.map((todo) => {
-    if (todo.id === id) return targetTodo;
-  });
-  res.status(200).json(targetTodo);
+app.patch('/api/todos/:id', (req, res) => {
+  const parsedId = parseInt(req.params.id);
+  if (isNaN(parsedId)) return res.status(400).send({ message: 'Bad Request. Invalid ID.' });
+
+  const findTodoIndex = todos.findIndex((todo) => todo.id === parsedId);
+  if (findTodoIndex === 1) return response.sendStatus(404);
+
+  todos[findTodoIndex] = { ...todos[findTodoIndex], ...req.body };
+  res.sendStatus(200);
 });
 
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id;
-  const todo = todos.find((todo) => todo.id === id);
+app.delete('/api/todos/:id', (req, res) => {
+  const parsedId = parseInt(req.params.id);
+  if (isNaN(parsedId)) return res.status(400).send({ message: 'Bad Request. Invalid ID.' });
 
-  if (todo) {
-    todos = todos.filter((todo) => todo.id !== id);
-    res.status(200).end();
-  } else
-    res.status(404).json({ error: 'Todo not found', message: '해당 todo를 찾을 수 없습니다.' });
+  const findTodoIndex = todos.findIndex((todo) => todo.id === parsedId);
+  if (findTodoIndex === 1) return response.sendStatus(404);
+
+  todos.splice(findTodoIndex, 1);
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
